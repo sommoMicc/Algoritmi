786e87a3a5c42f5d1aae409c2da8deb07677b014
Michele Tagliabue <mikitg.michele@gmail.com>
2019-03-28 12:07:35 +0100

    Rimossi file e codice inutili, aggiunti commenti

--- esercizio1/svolgimento/helper/graphAttacker.js
+++ esercizio1/svolgimento/helper/graphAttacker.js
@@ -2,24 +2,29 @@ const GraphWalker = require("./graphWalker");
 
 module.exports = class GraphAttacker {
     /**
-     * @param {UndirectedGraph} graph il grafo da disattivare
+     * Esegue l'attacco random sui nodi del grafo "graph"
+     * @param {UndirectedGraph} graph il grafo da attaccare
+     * @param {Object} progressWatcher observer per l'avanzamento
      */
     static randomFullDeactivation(graph,progressWatcher) {
         const results = [];
-
+        //Mescolo i nodi del grafo in modo tale da disattivarli in ordine casuale
         const nodesToDeactivate = GraphAttacker._shuffleArray(graph.getNodes());
         results[0] = GraphWalker.maxConnectedComponents(graph);
 
         for(let i=0;i<nodesToDeactivate.length;i++) {
+            //Disconnetto il nodo attaccato da tutti i suoi archi
             graph.disconnectNode(nodesToDeactivate[i]);
             
+            //Calcolo la nuova dimensione della componente connessa massima
             const maxConnectedComponent = GraphWalker.maxConnectedComponents(graph);
             results[i+1] = maxConnectedComponent;
             
-            
+            //Calcolo il nuovo progresso dell'elaborazione
             let progress = Math.round(i*100/nodesToDeactivate.length);
             let progressInterval = i % (Math.round(nodesToDeactivate.length / 100));
-
+            //Se sono avanzato di una percentuale, lo notifico al processo principale
+            //in modo da aggiornare la barra di avanzamento
             if(progressInterval == 0) {
                 progressWatcher.onProgress(progress);
             }
@@ -28,6 +33,10 @@ module.exports = class GraphAttacker {
         return results;
     }
 
+    /**
+     * Mescola gli elementi di un array
+     * @param {*} array l'array da mescolare
+     */
     static _shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -36,27 +45,36 @@ module.exports = class GraphAttacker {
         return array;
     }
 
+    /**
+     * Attua l'attacco intelligente al grafo passato come parametro
+     * @param {UndirectedGraph} graph il grafo da attaccare
+     * @param {Object} progressWatcher observer per l'avanzamento
+     */
     static cleverFullDeactivation(graph,progressWatcher) {
         const results = [];
         results[0] = GraphWalker.maxConnectedComponents(graph);
         
+        //Ottengo la lista dei nodi del grafo ordinata per grado decrescente
+        //in modo da disattivare prima i nodi con un grado elevato
         let nodesToDeactivate = graph.getNodesOrderedByGrade();
 
         for(let i=0;i<nodesToDeactivate.length;i++) {
+            //Disconnetto il nodo attaccato da tutti i suoi archi
             graph.disconnectNode(nodesToDeactivate[i].node);
-            
+
+            //Calcolo la nuova dimensione della componente connessa massima
             const residualConnectedComponent = GraphWalker.maxConnectedComponents(graph);
             results[i+1] = residualConnectedComponent;
-            
+
+            //Calcolo il nuovo progresso dell'elaborazione
             let progress = Math.round(i*100/nodesToDeactivate.length);
             let progressInterval = i % (Math.round(nodesToDeactivate.length / 100));
-
+            //Se sono avanzato di una percentuale, lo notifico al processo principale
+            //in modo da aggiornare la barra di avanzamento
             if(progressInterval == 0) {
                 progressWatcher.onProgress(progress);
             }
         }
-
-        //console.log(results);
 
         return results;
     }
--- esercizio1/svolgimento/helper/graphWalker.js
+++ esercizio1/svolgimento/helper/graphWalker.js
@@ -1,4 +1,3 @@
-const Queue = require("../models/queue");
 module.exports = class GraphWalker {
     static get Colors() {
         return {
@@ -7,61 +6,6 @@ module.exports = class GraphWalker {
             BLACK: "black"
         }
     };
-
-    /**
-     * @param {UndirectedGraph} G il grafico 
-     * @param {Number} s indice del nodo da cui iniziare il cammino
-     */
-    static BFS(G,s) {
-
-        let d = [];
-        let color = [];
-        let predecessor = [];
-
-        //console.log(G.getNodes());
-        G.getNodes().forEach((v)=>{
-            d[v] = Infinity;
-            color[v] = GraphWalker.Colors.WHITE
-            predecessor[v] = null;
-        });
-
-        console.log("D length: "+d.length);
-
-        color[s] = GraphWalker.Colors.GRAY;
-        d[s] = 0;
-
-        let Q = new Queue();
-        Q.enqueue(s);
-
-        while(!Q.empty()) {
-            let u = Q.dequeue();
-            try {
-                let adj = G.adjacence(u);
-                if(adj != null)
-                    Object.keys(adj).forEach((v)=>{
-                        if(v > d.length) {
-                            console.log("V: "+v+", d: "+d);
-                        }
-                        if(color[v] == GraphWalker.Colors.WHITE) {
-                            color[v] = GraphWalker.Colors.GRAY;
-                            d[v] = d[u] + 1;
-                            predecessor[v] = u;
-                            Q.enqueue(v);
-                        }
-                    });
-            }
-            catch(e) {
-                console.error(e);
-                console.log("U: "+u);
-                console.log(G.adjacence(u));
-            }
-            color[u] = GraphWalker.Colors.BLACK;
-        }
-        return {
-            d: d,
-            predecessor: predecessor
-        }
-    }
 
     /**
      * Ritorna l'insieme delle componenti connesse di G
@@ -84,7 +28,13 @@ module.exports = class GraphWalker {
         }
         return CC;
     }
-
+    /**
+     * Implementazione della procedura DFS_Visited
+     * @param {*} G il grafo che si sta visitando
+     * @param {*} u il nodo di partenza
+     * @param {*} visited la lista dei nodi già visitati partendo da u
+     * @param {*} color array che assegna ad ogni nodo del grafo il suo colore
+     */
     static _DFS_Visited(G,u,visited,color) {
         color[u] = GraphWalker.Colors.GRAY;
         visited.push(u);
