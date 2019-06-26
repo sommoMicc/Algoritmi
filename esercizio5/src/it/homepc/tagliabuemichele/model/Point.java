603b9b893091e7a2e4aa715b24526065055dcb35
Michele Tagliabue <mikitg.michele@gmail.com>
2019-06-26 20:57:41 +0200

    Distanza geo

--- esercizio5/src/it/homepc/tagliabuemichele/model/Point.java
+++ esercizio5/src/it/homepc/tagliabuemichele/model/Point.java
@@ -26,9 +26,32 @@ public class Point {
         this.lng = lng;
     }
 
-    public double distance(Point b) {
-        return Math.sqrt((Math.pow((lat - b.lat),2)) + (Math.pow((lng - b.lng),2)));
 
+    public double distance(Point b) {
+        return Point.geoDistance(lat,b.lat,lng,b.lng);
+    }
+
+    /**
+     * Calculate distance between two points in latitude and longitude taking
+     * into account height difference. If you are not interested in height
+     * difference pass 0.0. Uses Haversine method as its base.
+     *
+     * lat1, lon1 Start point lat2, lon2 End point el1 Start altitude in meters
+     * el2 End altitude in meters
+     * @returns Distance in Meters
+     */
+    private static double geoDistance(double lat1, double lat2, double lon1,
+                                  double lon2) {
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return R * c * 1000; // convert to meters
     }
 
     @Override
