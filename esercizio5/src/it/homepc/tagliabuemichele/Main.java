b21ae856a56df5a94e30ce4266883546300d5810
Michele Tagliabue <mikitg.michele@gmail.com>
2019-06-29 19:42:17 +0200

    Aggiunto calcolo risposta alla domanda 2

--- esercizio5/src/it/homepc/tagliabuemichele/Main.java
+++ esercizio5/src/it/homepc/tagliabuemichele/Main.java
@@ -19,23 +19,29 @@ public class Main {
         0, 250, 2000, 5000, 15000, 50000, 100000
     };
 
+    private static boolean DOMANDA_1 = false;
+    private static boolean DOMANDA_2 = true;
+
     private static boolean ESECUZIONE_SERIALE = true;
     private static boolean ESECUZIONE_PARALLELA = true;
 
 
     public static void main(String[] args) {
+        KMeans kmeans = null;
+        NoobPKMeans pkmeans = null;
 
+        List<Cluster> kmeansResult = null;
+        List<Cluster> pKmeansResult = null;
+
+        if(DOMANDA_1)
         for(int i=0;i<filters.length;i++) {
             System.out.println("-----------------------------------");
             List<City> cities = CityController
                     .getInstance().readCitiesWithFilter(filters[i]);
 
-            KMeans kmeans = null;
-            NoobPKMeans pkmeans = null;
-
             if(ESECUZIONE_SERIALE) {
                 kmeans = new KMeans(cities, 50, 100);
-                List<Cluster> kmeansResult = kmeans.start();
+                kmeansResult = kmeans.start();
 
                 System.out.println("Distorsione con filtro " + filters[i] + ": " +
                         decimalFormat.format(Cluster.distortion(kmeansResult)) + " in " +
@@ -44,16 +50,54 @@ public class Main {
             }
             if(ESECUZIONE_PARALLELA) {
                 pkmeans = new NoobPKMeans(cities, 50, 100);
-                List<Cluster> pKmeansResult = pkmeans.start();
+                pKmeansResult = pkmeans.start();
 
                 System.out.println("Distorsione parallela con filtro " + filters[i] + ": " +
                         decimalFormat.format(Cluster.distortion(pKmeansResult)) + " in " +
                         pkmeans.getElapsedTime() + " secondi");
             }
-            if(ESECUZIONE_PARALLELA && ESECUZIONE_SERIALE)
-                System.out.println("Speedup: "+kmeans.getElapsedTime()*1.0/pkmeans.getElapsedTime());
+            if(ESECUZIONE_PARALLELA && ESECUZIONE_SERIALE) {
+                double speedup = kmeans.getElapsedTime() * 1.0 / pkmeans.getElapsedTime();
+                System.out.println("Speedup: " +speedup);
+                System.out.println("Speedup per core: " +speedup /
+                    Runtime.getRuntime().availableProcessors());
+            }
         }
 
+        if(DOMANDA_2)
+        for(int cluster = 10; cluster <= 100; cluster++) {
+            System.out.println("-----------------------------------");
+            List<City> cities = CityController
+                    .getInstance().readCities();
+
+
+            if(ESECUZIONE_SERIALE) {
+                kmeans = new KMeans(cities, cluster, 100);
+                kmeansResult = kmeans.start();
+
+                System.out.println("KMeans su "+cluster+" cluster ha concluso in " +
+                        kmeans.getElapsedTime() + " secondi");
+
+            }
+            if(ESECUZIONE_PARALLELA) {
+                pkmeans = new NoobPKMeans(cities, cluster, 100);
+                pKmeansResult = pkmeans.start();
+
+                System.out.println("P-KMeans su "+cluster+" cluster ha concluso in " +
+                        pkmeans.getElapsedTime() + " secondi");
+            }
+            if(ESECUZIONE_PARALLELA && ESECUZIONE_SERIALE) {
+
+                if(Cluster.distortion(kmeansResult) != Cluster.distortion(pKmeansResult)) {
+                    System.out.println("Attenzione: DISTORSIONI DIVERSE!!!");
+                }
+
+                double speedup = kmeans.getElapsedTime() * 1.0 / pkmeans.getElapsedTime();
+                System.out.println("Speedup: " +speedup);
+                /*System.out.println("Speedup per core: " +speedup /
+                        Runtime.getRuntime().availableProcessors());*/
+            }
+        }
 
     }
 }
